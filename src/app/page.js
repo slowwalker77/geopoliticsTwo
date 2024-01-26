0cbabab5a9021b40ed9db42ad746dc9843549404
slowwalker77 <90623845+slowwalker77@users.noreply.github.com>
2024-01-27 00:21:54 +0900

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,7 @@
-import { redirect } from 'next/navigation';
-
 export default function Home() {
-  <span className='sr-obly'>
-    Home 페이지에 내용 넣기 전까지 국내문제로 redirect
-  </span>;
-  redirect('/blogs/section-1');
-  return;
+return(
+  <div>
+  지정학과 세상읽기
+  </div>
+)
 }
