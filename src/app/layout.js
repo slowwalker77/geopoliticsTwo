db8aea3f2550f1933518089c4d4247008ed0395f
slowwalker77 <90623845+slowwalker77@users.noreply.github.com>
2024-01-27 10:33:30 +0900

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,17 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from 'next/font/google';
+import './globals.css';
 import Header from '../components/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Personel Steemit Blog',
-  description: 'Personel Steemit Blog',
+  title: '지정학과 세상읽기',
+  description: '한국의 입장에서 본 지정학과 인민의 삶을 위한 정치',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={inter.className} suppressHydrationWarning={true}>
         <Header />
         {children}
