675aff2563847df4b9cddd7083e83fbd88eb1291
slowwalker77 <90623845+slowwalker77@users.noreply.github.com>
2025-02-12 12:59:16 +0900

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>대한민국의 바람직한 미래를 위한 고민을 찾아서</div>;
+  return (
+    <div style={{ padding: '0 20px' }}>
+      <div>대한민국의 바람직한 미래를 위한 고민을 찾아서</div>
+    </div>
+  );
 }
